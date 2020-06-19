706b8205d17320a9416e69451129dee6b42d6aaf
park <dcloyal1@gmail.com>
2020-06-19 21:10:08 +0900

    Add Customer List Refresh

--- client/src/App.js
+++ client/src/App.js
@@ -28,10 +28,24 @@ const styles = theme => ({
 
 class App extends Component {
 
-  state = {
-    customers: "",
-    completed: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: '',
+      completed: 0
+    }
   }
+
+  stateRefresh = () => {
+    this.setState({
+      customers: '',
+      completed: 0
+    });
+    this.callApi()
+     .then(res => this.setState({ customers: res }))
+     .catch(err => console.log(err));
+  }
+
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
@@ -85,7 +99,7 @@ class App extends Component {
           </TableBody>
         </Table>
       </Paper>
-      <CustomerAdd/>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
       </div>    
     );
   }
